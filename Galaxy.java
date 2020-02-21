5f7db5518025229bf29a8841245eb3318d7b8139
rashimahendra <60163773+rashimahendra@users.noreply.github.com>
2020-02-21 00:00:14 -0700

    Update Galaxy.java

--- Galaxy.java
+++ Galaxy.java
@@ -1,12 +1,31 @@
 
 public class Galaxy {
 	
-  	private final String PLANET_TYPE = "terrestrial";
+  	private String galaxyName;
+	private String starName;
 	
-  public Galaxy() {
-	planet();
-	stars();
-	  // call classes in galaxy
+        public Galaxy() {
 				}
-
+	public Galaxy( String aGalaxyName, String aStarName) {
+		this.galaxyName = aGalaxyName;
+	        this.starName = aStarName;}
+	
+	public String setStarInfo(String aStarName){
+		this.starName = aStarName;}
+	// need a hashmap here
+	
+        public String getStarInfo(){
+	return starName + info;
+	// info is a variable for the info got from hashmap
+	}
+	
+	public String setGalaxyInfo(String aGalaxyName){
+		this.galaxyName = aGalaxyName;}
+	// need a hashmap here
+	
+	public String getGalaxyInfo(){
+	return galaxyName + info;
+	// info is a variable for the info got from hashmap
+	}
+	
 }
