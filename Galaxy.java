fdf6ee2c82743d2853c5d6d56ebb1e8cab900a11
rashimahendra <60163773+rashimahendra@users.noreply.github.com>
2020-02-27 12:43:25 -0700

    Update Galaxy.java

--- Galaxy.java
+++ Galaxy.java
@@ -4,25 +4,33 @@ public class Galaxy {
   	private String galaxyName;
 	private String starName;
 	
+	// Default constructor 
         public Galaxy() {
 				}
+	// Copy constructor for galaxy name and star name
 	public Galaxy( String aGalaxyName, String aStarName) {
 		this.galaxyName = aGalaxyName;
 	        this.starName = aStarName;}
 	
-	public String setStarInfo(String aStarName){
+	// Setter method for the star info 
+	public void setStarInfo(String aStarName){
 		this.starName = aStarName;}
 	// need a hashmap here
 	
+	 /* Getter method for the star info
+	Returns the star name and star info /*
         public String getStarInfo(){
 	return starName + info;
 	// info is a variable for the info got from hashmap
 	}
 	
-	public String setGalaxyInfo(String aGalaxyName){
+	// Setter method for the galaxy info
+	public void setGalaxyInfo(String aGalaxyName){
 		this.galaxyName = aGalaxyName;}
 	// need a hashmap here
 	
+	/* Getter method for the galaxy info
+	Returns the galaxy name and galaxy info /*
 	public String getGalaxyInfo(){
 	return galaxyName + info;
 	// info is a variable for the info got from hashmap
