42bb54c90720e386d4bd0365352eba81c5c51d6b
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-02-27 12:31:10 -0700

    UpdatedMain

--- Main.java
+++ Main.java
@@ -1,130 +1,148 @@
-import java.lang.reflect.Method;
-import java.util.*;
-public class Main {
+import java.util.ArrayList;
 
-		private Universe myUniverse;    // itemCollection
-		private HashMap< String , String > myMenus = new HashMap<>(); // HashMap of Menus
-		
-		
+/*****************************
+ * class NonProjectCategory
+ * 
+ * This class represents any category of grades used for the course that are not part of the 
+ * project grade. 
+ * It contains the following instance variables:
+ * 	numDroppedGrades: an integer indicating how many grades will be dropped from the category's 
+ *     assessments before calculating a weighted average
+ *
+ */
+
+public class NonProjectCategory extends Category {
+	private int numDroppedGrades = 0;
 	
-		
-		
-		/*
-		 * Main constructor
-		 */
-		public Main() {
-			
-		}
-		
+	/* NonProjectCategory(String, double, int, double[])
+	 * Constructor for the class
+	 * 
+	 * @param newName the name that will be used for the new instance of the class
+	 * @param newWeight the weight that will be used for the new instance of the class
+	 * @param newNumGradesToDrop the number of grades to be dropped by the new instance of the class
+	 * @param rawGrades the individual grades that will be used for the new instance of the class
+	 * 	Ignore grades that are invalid (for instance, if they are less than 0 or greater than 4.3)
+	 * 
+	 * TODO: Implement the body of this method (hint: it shouldn't be more than two lines)
+	 * 
+	 * You may wish to note how other methods are called to help you implement this.
+	 */
 	
+	public NonProjectCategory(String newName, double newWeight, int newNumGradesToDrop, double [] rawGrades)
 		
-		
-		public void titlePage() {
-			
-			System.out.println(" ___     ________    __________   ______");
-			System.out.println("|   \\   |      _|   |__     ___| |      \\");
-			System.out.println("|    \\  |     |         |   |    |       |   ");	
-			System.out.println("| /\\  |  \\     \\        |   |  |       |");
-			System.out.println("| __  |   |     |       |   |     |       |");
-			System.out.println("|  _  | __|     |       |   |     |       |");
-			System.out.println("|_| |_| |_______|       |___|     |_______|" + "\npress any key to start");
-			
-			
-			
+	{
+		super(newName,newWeight,rawGrades);
+		this.setNumDroppedGrades(newNumGradesToDrop);
+	}
+	
+	/* calculateGrade()
+	 * DO NOT MODIFY 
+	 * 
+	 * @return a double which is the weighted grade for the category
+	 * 
+	 * If the weight is invalid (less than 0 or greater than 1.0), then returns
+	 * 0.0
+	 * 
+	 */
+	
+	public double calculateGrade()
+	{
+		boolean validWeight = ((this.weight > 0) && (this.weight <= 1.0));
+		if(!grades.isEmpty() && validWeight)
+		{
+			return findAverageGrade(dropGrades() )* this.weight;
+		}
+		else 
+			return 0.0;
+	}
+	
+	/* dropNGrades()
+	 * @param gradeList an ArrayList of Doubles which are all the grades to consider
+	 * @param numberToDrop an int representin the number of grades to drop from the result
+	 * @return an ArrayList of Doubles which are all the grades which were not dropped
+	 * 
+	 * If the list after dropping all grades is empty, then add a single double with a 0.0 
+	 * grade to the list
+	 * 
+	 * Make the following modifications:
+	 *  - rename the method to dropGrades()
+	 *  - remove all arguments from the method signature. Instead of these arguments, the method
+	 *    should use only instance variables
+	 *  - modify this method however you need to run it succesfully
+	 *  - update this comment block to match your modifications
+	 * 
+	 */
+	
+	public ArrayList<Double> dropGrades( )
+	{
+		ArrayList<Double> droppedList = new ArrayList<Double>();
+		for(Double grade: this.getGrades())
+		{
+			droppedList.add(new Double(grade));
 		}
 		
-		public void putClassInMap( HashMap<String, String> aMap  , String aKey, String aMenu) {
-			
-			aMap.put( aKey, aMenu );
-			
-		}
-		
-		
-		
-		public void drawSquares(Grid g) {
-			
-			for(int i = 0 ; i < g.getRowSize(); i++) {
-				
-				for(int j = 0; j < g.getColumnSize(); j++) {
-					
-					g.getGrid()[i][i] = g.getSymbol();
-					
+		int dropCounter = 0;
+		while((this.getNumDroppedGrades() <= droppedList.size()) && (dropCounter < this.getNumDroppedGrades())) //Fix this code
+		{
+			int minIndex = 0;		
+			for(int itemCounter = 0; itemCounter < droppedList.size(); itemCounter++)
+			{
+				if (-1 == droppedList.get(itemCounter).compareTo(droppedList.get(minIndex).doubleValue()))
+				{
+					minIndex = itemCounter;
 				}
 			}
-			
+			droppedList.remove(minIndex);
+			dropCounter++;
 		}
 		
-		public String displayGridMenu() {
-			Grid g = new Grid(50,50, "*");
-			return g.outputGrid();
+		return droppedList;
+	}
+	
+	/* findAverageGrade(ArrayList<Double)
+	 * DO NOT MODIFY 
+	 * 
+	 * @param a provided list of grades
+	 * @return a double which is the average grade for the list passed in as an argument
+	 * 
+	 * If the weight is invalid (less than 0 or greater than 1.0), then returns
+	 * 0.0
+	 * 
+	 */
+	
+	public double findAverageGrade(ArrayList<Double> rawGrades)
+	{
+		double mean = 0.0;
+		double sum = 0.0;
+		
+		for (int gradeCounter = 0; gradeCounter < rawGrades.size(); gradeCounter++)
+		{
+			sum += rawGrades.get(gradeCounter).doubleValue();
 		}
 		
-		public void displayMenu( HashMap< String , String > myMenu , String theKey, int a ) { //Ignore "int a" , will replace after.
-			
-			
+		mean = sum / rawGrades.size();
+		
+		if ((mean < 0.0) || (mean > 4.0))
+		{
+			mean = 0.0;
 		}
-		
-		public void searchForMethod(HashMap<String, Class<?>> map, String aKey) {
-			
-			Method determinedMenuMethod = null;
-			Class<?> myClass = map.get(aKey);
-			
-			for( Method method: myClass.getMethods()) {
+		// include a check: if your mean is greater than 4.0 or less than 0.0
+		// have the method return 0
 				
-				
-				
-				if( method.getName().contentEquals("displayMenu") ) {
-					
-					System.out.println("Method found.");
-					
-					
-				}
-				
-			}
-			
-			
-		}
-		
-		
-		
-		public String getMenuOption() {
-			return "";
-		}
-		
-		
-		
-		public static void main(String[] args) {
-			
-			
-			Main textBasedApp = new Main();
-			Scanner waitForButton = new Scanner(System.in);
-			textBasedApp.titlePage();
-			String keyPressed = waitForButton.nextLine();
-			
-		
-			textBasedApp.myUniverse = new Universe();
-			
-			System.out.println(textBasedApp.myUniverse.displayMenu());
-			Scanner userInput = new Scanner(System.in);
-			System.out.println("What galaxy would you like to visit? : ");
-			String userAnswer = userInput.nextLine();
-			
-			while( textBasedApp.myUniverse.getGalaxies().containsKey(userAnswer) == false ) { //Checks if userAnswer is in HashMap of classes.
-				
-				System.out.println("Class doesn't exist, try again. ");
-				userAnswer = userInput.nextLine();
-			}
-			
-			if( textBasedApp.myUniverse.getGalaxies().containsKey(userAnswer) ) {
-				System.out.println("Class found!");
-				Class<?> currentClass = textBasedApp.myUniverse.getGalaxies().get(userAnswer);
-				
-				
-			}
-			else {
-				System.out.println("That class doesn't exist. Try again.");
-			}
-			 
-			
-			}
+		return mean;
+	}
+
+	
+	// getters and setters
+	
+	public int getNumDroppedGrades() {
+		return numDroppedGrades;
+	}
+
+	public void setNumDroppedGrades(int numDroppedGrades) {
+		this.numDroppedGrades = numDroppedGrades;
+	}
+	
+	
+
 }
