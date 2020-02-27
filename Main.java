917cd1c426c07b5315b167a559455ccaf2bb390f
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-02-27 12:31:38 -0700

    UpdatedMain

--- Main.java
+++ Main.java
@@ -1,148 +1,290 @@
-import java.util.ArrayList;
-
-/*****************************
- * class NonProjectCategory
+import java.lang.reflect.Method;
+import java.util.*;
+/*
  * 
- * This class represents any category of grades used for the course that are not part of the 
- * project grade. 
- * It contains the following instance variables:
- * 	numDroppedGrades: an integer indicating how many grades will be dropped from the category's 
- *     assessments before calculating a weighted average
- *
+ * The Main class is in charge of all the menu option handling and transitions between classes.
+ * 
+ * 
  */
+public class Main {
 
-public class NonProjectCategory extends Category {
-	private int numDroppedGrades = 0;
-	
-	/* NonProjectCategory(String, double, int, double[])
-	 * Constructor for the class
-	 * 
-	 * @param newName the name that will be used for the new instance of the class
-	 * @param newWeight the weight that will be used for the new instance of the class
-	 * @param newNumGradesToDrop the number of grades to be dropped by the new instance of the class
-	 * @param rawGrades the individual grades that will be used for the new instance of the class
-	 * 	Ignore grades that are invalid (for instance, if they are less than 0 or greater than 4.3)
-	 * 
-	 * TODO: Implement the body of this method (hint: it shouldn't be more than two lines)
-	 * 
-	 * You may wish to note how other methods are called to help you implement this.
-	 */
-	
-	public NonProjectCategory(String newName, double newWeight, int newNumGradesToDrop, double [] rawGrades)
+		private Universe myUniverse;    // itemCollection
+		private HashMap< String , String > myMenus = new HashMap<>(); // HashMap of Menus
+		private String currentMenuOption;
+		private ArrayList<String> menuOptions = new ArrayList<String>();
+		private String currentMenu;
 		
-	{
-		super(newName,newWeight,rawGrades);
-		this.setNumDroppedGrades(newNumGradesToDrop);
-	}
+		
 	
-	/* calculateGrade()
-	 * DO NOT MODIFY 
-	 * 
-	 * @return a double which is the weighted grade for the category
-	 * 
-	 * If the weight is invalid (less than 0 or greater than 1.0), then returns
-	 * 0.0
-	 * 
-	 */
-	
-	public double calculateGrade()
-	{
-		boolean validWeight = ((this.weight > 0) && (this.weight <= 1.0));
-		if(!grades.isEmpty() && validWeight)
-		{
-			return findAverageGrade(dropGrades() )* this.weight;
-		}
-		else 
-			return 0.0;
-	}
-	
-	/* dropNGrades()
-	 * @param gradeList an ArrayList of Doubles which are all the grades to consider
-	 * @param numberToDrop an int representin the number of grades to drop from the result
-	 * @return an ArrayList of Doubles which are all the grades which were not dropped
-	 * 
-	 * If the list after dropping all grades is empty, then add a single double with a 0.0 
-	 * grade to the list
-	 * 
-	 * Make the following modifications:
-	 *  - rename the method to dropGrades()
-	 *  - remove all arguments from the method signature. Instead of these arguments, the method
-	 *    should use only instance variables
-	 *  - modify this method however you need to run it succesfully
-	 *  - update this comment block to match your modifications
-	 * 
-	 */
-	
-	public ArrayList<Double> dropGrades( )
-	{
-		ArrayList<Double> droppedList = new ArrayList<Double>();
-		for(Double grade: this.getGrades())
-		{
-			droppedList.add(new Double(grade));
+		
+		
+		/*
+		 * Main constructor
+		 */
+		public Main() {
+			Galaxy galaxy = new Galaxy();
+			MilkyWay milky = new MilkyWay();
+			Planet pl = new Planet();
+			Earth earth = new Earth();
+			Stars stars = new Stars();
+			
+			String GalaxyMenuOutput = "THIS IS THE UNIVERSE MENU";
+			String output0 = " + + + + + + + + + + + + + + + + + + + + + + + + + + + + +";
+			String output1 = " +  -----------         -----------                      +";
+			String output2 = " + | Milky Way  |      | Andromeda |                     + " ;
+			String output3 = " +  -----------         -----------                      +";
+			String outputa = " +                                                       +";
+			String outputb = " +                                                       +";
+			String outputc = " +                                                       +";
+			String outputd = " +                                                       +";
+			String outpute = " +                                                       +";
+			String outputf = " +                                                       +";
+			String outputg = " +                                                       +";
+			String output4 = " + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ";
+			String output5 = " Which galaxy would you like to visit? ";
+			GalaxyMenuOutput = output0 + "\n" + output1 + "\n" + output2 + "\n" + output3 + "\n" + output4 + "\n" + output5;
+			
+			this.menuOptions.add("Galaxy");
+			this.menuOptions.add("Milky Way");
+			this.menuOptions.add("Andromeda");
+			this.menuOptions.add("Planets");
+			this.menuOptions.add("Stars");
+			this.menuOptions.add("Earth");
+			this.menuOptions.add("Mercury");
+			this.menuOptions.add("Venus");
+			this.menuOptions.add("Mars");
+			this.menuOptions.add("Jupiter");
+			this.menuOptions.add("Saturn");
+			
+			this.getMenus().put("Milky Way", milky.displayMenu());
+			this.getMenus().put("Stars" , stars.displayMenu());
+			this.getMenus().put("Earth " , earth.getPlanetDescription());
+			this.getMenus().put("Planets" , pl.displayMenu());
+			
+			this.getMenus().put("Planets", GalaxyMenuOutput);
+			
+		
 		}
 		
-		int dropCounter = 0;
-		while((this.getNumDroppedGrades() <= droppedList.size()) && (dropCounter < this.getNumDroppedGrades())) //Fix this code
-		{
-			int minIndex = 0;		
-			for(int itemCounter = 0; itemCounter < droppedList.size(); itemCounter++)
-			{
-				if (-1 == droppedList.get(itemCounter).compareTo(droppedList.get(minIndex).doubleValue()))
-				{
-					minIndex = itemCounter;
+		public String displayMenu(String aKeyWord) {
+			String desiredMenu = "";
+			
+			for( String menuOption: this.menuOptions ) {
+				
+				if(  menuOption.equalsIgnoreCase(aKeyWord) ) {
+					desiredMenu = this.getMenus().get(menuOption);
+				}
+				else {
+					return null;
 				}
 			}
-			droppedList.remove(minIndex);
-			dropCounter++;
+			
+			return desiredMenu;
 		}
 		
-		return droppedList;
-	}
+		public HashMap<String , String> getMenus(){
+			
+			return this.myMenus;
+		}
+		
+		public String getCurrentMenuOption() {
+			
+			return this.currentMenuOption;
+		}
+		
+		public void updateCurrentMenuOption(String aMenuOption) {
+			this.currentMenuOption = aMenuOption;
+		}
 	
-	/* findAverageGrade(ArrayList<Double)
-	 * DO NOT MODIFY 
-	 * 
-	 * @param a provided list of grades
-	 * @return a double which is the average grade for the list passed in as an argument
-	 * 
-	 * If the weight is invalid (less than 0 or greater than 1.0), then returns
-	 * 0.0
-	 * 
-	 */
-	
-	public double findAverageGrade(ArrayList<Double> rawGrades)
-	{
-		double mean = 0.0;
-		double sum = 0.0;
-		
-		for (int gradeCounter = 0; gradeCounter < rawGrades.size(); gradeCounter++)
-		{
-			sum += rawGrades.get(gradeCounter).doubleValue();
+		public String goToPreviousMenu() {
+			String prevMenu = "";
+			
+			return prevMenu;
 		}
 		
-		mean = sum / rawGrades.size();
-		
-		if ((mean < 0.0) || (mean > 4.0))
-		{
-			mean = 0.0;
+		public void titlePage() {
+			String star = "+";
+			System.out.println("+ + + + + + + + + + + + + + + + + + + + + + + + + ");
+			System.out.println(star + " ___     ________    __________   ______  " + star);
+			System.out.println(star +"|   \\   |      _|   |__     ___| |      \\  "+ star);
+			System.out.println(star+"|    \\  |     |         |   |    |       |    " + star);	
+			System.out.println(star +"| /\\  |  \\     \\        |   |    |       |  " + star);
+			System.out.println(star + "| __  |   |     |       |   |     |       |"+ star);
+			System.out.println(star + "|  _  | __|     |       |   |     |       |"+ star);
+			System.out.println(star +"|_| |_| |_______|       |___|     |_______|" + star +  "\npress any key to start");
+			
+			
+			
 		}
-		// include a check: if your mean is greater than 4.0 or less than 0.0
-		// have the method return 0
+		
+		public String createScanner(String scannerTopic) {
+		
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter the  " + scannerTopic);
+			String  output = input.nextLine();
+			
+			return output;
+		}
+		
+		public void putClassInMap( HashMap<String, String> aMap  , String aKey, String aMenu) {
+			
+			aMap.put( aKey, aMenu );
+			
+		}
+		
+		
+		
+		public void drawSquares(Grid g) {
+			
+			for(int i = 0 ; i < g.getRowSize(); i++) {
 				
-		return mean;
-	}
-
-	
-	// getters and setters
-	
-	public int getNumDroppedGrades() {
-		return numDroppedGrades;
-	}
-
-	public void setNumDroppedGrades(int numDroppedGrades) {
-		this.numDroppedGrades = numDroppedGrades;
-	}
-	
-	
-
+				for(int j = 0; j < g.getColumnSize(); j++) {
+					
+					g.getGrid()[i][i] = g.getSymbol();
+					
+				}
+			}
+			
+		}
+		
+		public String displayGridMenu() {
+			Grid g = new Grid(50,50, "*");
+			return g.outputGrid();
+		}
+		
+		
+		
+		
+		public void searchForMethod(HashMap<String, Class<?>> map, String aKey) {
+			
+			Method determinedMenuMethod = null;
+			Class<?> myClass = map.get(aKey);
+			
+			for( Method method: myClass.getMethods()) {
+				
+				
+				
+				if( method.getName().contentEquals("displayMenu") ) {
+					
+					System.out.println("Method found.");
+					
+					
+				}
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		public String getMenuOption() {
+			return "";
+		}
+		
+		
+		
+		public static void main(String[] args) {
+			
+			
+			Main textBasedApp = new Main();
+			Scanner waitForButton = new Scanner(System.in);
+			textBasedApp.titlePage();
+			String keyPressed = waitForButton.nextLine();
+			
+		
+			textBasedApp.myUniverse = new Universe();
+			
+			
+			
+			textBasedApp.myUniverse.displayMenu();
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("What galaxy would you like to visit? : ");
+			String userAnswer = userInput.nextLine();
+			
+			while( textBasedApp.myUniverse.getGalaxies().containsKey(userAnswer) == false ) { //Checks if userAnswer is in HashMap of classes.
+				
+				System.out.println("Does not exist, try again ");
+				userAnswer = userInput.nextLine();
+			}
+			
+				/*
+				for ( String menuOption: textBasedApp.menuOptions) { //Loop through arraylist of menu options and updates current menu .
+					if( menuOption.equalsIgnoreCase(userAnswer)) {
+						
+						textBasedApp.updateCurrentMenuOption(userAnswer);
+					}
+				
+				}
+				System.out.println(textBasedApp.getMenus().get(userAnswer));
+				Scanner reader = new Scanner(System.in);
+				System.out.println("You are currently in " + textBasedApp.getCurrentMenuOption());
+				*/
+			
+			if( textBasedApp.myUniverse.getGalaxies().containsKey(userAnswer) ) {
+				System.out.println("Class found!");
+				Class<?> currentClass = textBasedApp.myUniverse.getGalaxies().get(userAnswer);
+				
+				
+				if ( currentClass == MilkyWay.class) {
+					MilkyWay milky = new MilkyWay();  // Cannot make an instance of a generic class since Java is statically typed. Open to other solutions.
+					milky.displayMenu();
+					Scanner reader = new Scanner(System.in);
+					System.out.println("Search for any object in the Milky Way");
+					String userIn = reader.nextLine();
+					
+					
+					
+					while( textBasedApp.myUniverse.getClasses().containsKey(userIn) == false ) {
+						System.out.println("Null, try again");
+						userIn = reader.nextLine();
+					}
+					Class<?> nextClass = textBasedApp.myUniverse.getClasses().get(userIn);
+					if ( nextClass == Planet.class ) {
+						Planet p = new Planet();
+						System.out.println(p.getPlanetDescription());
+						System.out.println(p.displayMenu());
+						Scanner planetReader = new Scanner(System.in);
+						System.out.println("What planet would you like to know about? :");
+						String planetUserInput = planetReader.nextLine();
+						
+						if (planetUserInput.equalsIgnoreCase("Earth")) {
+							Earth e = new Earth();
+							System.out.println(e.getPlanetDescription());
+						}
+						else {
+							System.out.println("Planet does not exist in the database");
+						}
+					}
+					
+					else if ( nextClass == Stars.class)  {
+						Stars s = new Stars();
+						System.out.println(s.displayMenu());
+					}
+					else {
+						System.out.println(" Null ");
+					}
+					
+				}
+				
+				else {
+					System.out.println("Doesn't exist");
+				}
+				
+			}
+			else {
+				
+				System.out.println("That class doesn't exist. Try again.");
+				
+				
+			}
+		
+			
+			
+			
+			 
+			}
 }
