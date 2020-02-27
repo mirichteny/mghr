a5c8162ca64c23ab7f7dd5099503c10f07f220b5
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-02-27 14:41:39 -0700

    UpdatedUniv

--- Stars.java
+++ Stars.java
@@ -1,12 +1,115 @@
-/*Another object class. Anyone who works on this class should implement properties of stars, ( luminosity, color, mass, surface temperature,
+/*Another object class. Anyone who works on this class should implement properties of stars, ( luminisity,
+	 * star type: (dwarf star or super giant? ) , etc.
+	 * 
+	 * 
+	 * 
+	 */
+import java.util.ArrayList;
+public class Stars {
+	
+	/*Another object class. Anyone who works on this class should implement properties of stars, ( luminosity, color, mass, surface temperature,
 	 * star type: (dwarf star or super giant? ) , etc. 
 	 * ( 3 maximum from the properties AND star type)
 	 * 
 	 * 
 	 */
-public class Stars {
+
 	
 
+	
+	
+	
+			
+	
+			
+
+
+			private final String ID = "0005";
+			private ArrayList<String> starTypes;
+			private String starType;
+			private double luminosity;
+			private ArrayList<String> starColors;
+			private String starColor;
+			private double radius;
+			private double mass;
+			
+			public Stars() { 
+				
+				this.starTypes = new ArrayList<String>();
+				this.starType = "";
+				this.luminosity = 0;
+				this.starColors = new ArrayList<String>();
+				this.addAll();
+				
+				
+				
+				
+				
+				
+			}
+			
+			
+			
+			public void setStarType(String aStarType) {
+					for( String star: this.getStarTypes()) {
+						if( aStarType.contentEquals(star)) {
+							this.starType = aStarType;
+						}
+						
+					}
+			}
+			
+			public void setLuminosity(double aLuminosity) {
+				this.luminosity = aLuminosity;
+			}
+			public void setStarColor(String aColor) {
+				this.starColor = aColor;
+			}
+			
+			public String getStarType() {
+				return this.starType;
+			}
+			
+			public void addAll() {
+				
+				this.getStarTypes().add("hypergiants");
+				this.getStarTypes().add("supergiants");
+				this.getStarTypes().add("giants");
+				this.getStarTypes().add("subgiants");
+				this.getStarTypes().add("subdwarfs");
+				this.getStarTypes().add("dwarfs");
+				this.getStarTypes().add("neutronstars");
+			}
+			
+			public double getLuminosity() {
+				
+				return this.luminosity;
+				
+			}
+			
+			public ArrayList<String> getStarTypes(){
+				ArrayList<String> copyStarTypes = new ArrayList<String>();
+				
+				for( String starType : this.starTypes) {
+					copyStarTypes.add(starType);
+					
+				}
+				
+				return copyStarTypes;
+			}
+	
+			public String displayMenu() {
+				
+				
+				String output = "";
+				
+				String out1 = "   ---------        -------------  ";
+				String out2 = "  | the Sun |      | Other Stars |";
+				String out3 = "   --------         -------------";
+				
+				output = out1 + "\n" + out2 + "\n" + out3;
+				return output;
+			}
 	
 	
 	
