606c27f335b6bf37387b68f1363768413726b5cb
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-02-29 00:51:20 -0700

    UpdatedFileHandler

--- FileHandler.java
+++ FileHandler.java
@@ -1,6 +1,12 @@
 import java.io.File;
 import java.util.Scanner;
 
+/*  The role of the filehandler class (not entirely implemented yet) is to take any file and gather data from that file. 
+*	
+*
+*
+*
+*/
 public class FileHandler {
 	
 		private Scanner myScanner;
