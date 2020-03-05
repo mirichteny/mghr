d9b9c1c3e5cebf7b0d9a481448cd7490c17bfe89
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-03-05 13:32:33 -0700

    UpdatedGalaxyMenuGUI

--- GalaxyMenuGUI.java
+++ GalaxyMenuGUI.java
@@ -1,7 +1,13 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GalaxyMenuGUI extends Application {
@@ -15,8 +21,17 @@ public class GalaxyMenuGUI extends Application {
 	
 	public void start(Stage primaryStage) throws Exception {
 	
+	FlowPane flowPane = new FlowPane();
+	VBox root = new VBox(10);
 	
-		
+	root.getChildren().addAll( new Button("Andromeda"), new Button("Milky Way"));
+	
+	flowPane.getChildren().add(root);
+	flowPane.setAlignment(Pos.CENTER);
+	
+	
+	primaryStage.setScene(new Scene(flowPane));
+	primaryStage.show();
 		
 			
 	}
