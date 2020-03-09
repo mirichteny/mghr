00474ce3f279f0fefc9340d5183459674368b3c2
rashimahendra <60163773+rashimahendra@users.noreply.github.com>
2020-03-08 19:47:39 -0600

    Update and rename Planet_GUI.java to Planets_GUI.java

---
+++ Planets_GUI.java
@@ -0,0 +1,39 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Button;
+
+/* This class is a GUI version of the Planets text based class which creates eight buttons 
+namely Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus and Neptune and aligns them to the center of the stage */
+public class Planets_GUI extends Application {
+  public static void main(String[] args)
+   {
+      launch(args);
+   }
+
+   
+   public void start(Stage primaryStage) throws Exception
+   {
+	   FlowPane root = new FlowPane();
+	   root.setVgap(20);
+	   root.setHgap(10);
+	   
+	   // adding all the buttons created
+	   root.getChildren().add(new Button("Mercury"));
+	   root.getChildren().add(new Button("Venus"));
+	   root.getChildren().add(new Button("Earth"));
+	   root.getChildren().add(new Button("Mars"));
+	   root.getChildren().add(new Button("Jupiter"));
+	   root.getChildren().add(new Button("Saturn"));
+	   root.getChildren().add(new Button("Uranus"));
+	   root.getChildren().add(new Button("Neptune"));
+
+	   Scene scene = new Scene(root, 500, 200);
+	   primaryStage.setTitle("Planets");
+	   primaryStage.setScene(scene);
+	   primaryStage.show();
+   }
+}
+
+}
