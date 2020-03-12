82470f0e7bf2ad9fd7158d8a2460848542b85718
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-03-11 21:37:51 -0600

    Created Controller Class (Unfinished)

---
+++ Controller.java
@@ -0,0 +1,25 @@
+import GUI.MainGUI;
+
+
+import LogicClasses.Universe;
+
+/*
+ * Controller class communicates with GUI and textBased logic.
+ * 
+ */
+
+public class Controller {
+	
+			private MainGUI myGUI;
+			private Universe myUniverse;
+			
+			
+			public String getUserRequestedSearch(String userInput) {
+				return "";
+			}
+			
+			public void update() {
+				
+			}
+			
+}
