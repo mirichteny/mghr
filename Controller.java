242f2fad5c7e463fcf8964d9edc3adab1e14364c
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-03-12 13:45:25 -0600

    UpdatedController

--- Controller.java
+++ Controller.java
@@ -1,25 +1,36 @@
+
 import GUI.MainGUI;
-
-
-import LogicClasses.Universe;
-
+import LogicClasses.*;
 /*
  * Controller class communicates with GUI and textBased logic.
  * 
  */
 
-public class Controller {
+
+
+public class MainController {
 	
 			private MainGUI myGUI;
 			private Universe myUniverse;
 			
+			public MainController() {
+				myGUI = new MainGUI();
+				myUniverse = new Universe();
+				
+				
+			}
+			
 			
 			public String getUserRequestedSearch(String userInput) {
-				return "";
+				return myUniverse.getItemByName(userInput);
 			}
 			
 			public void update() {
-				
+				myUniverse.updateScenes();
 			}
+			
+			
+			
+			
 			
 }
