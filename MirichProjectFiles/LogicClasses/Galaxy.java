f24ad83a1fc9c8300799ca9ec93297648fb1c34b
rashimahendra <60163773+rashimahendra@users.noreply.github.com>
2020-03-21 17:53:53 -0600

    Update Galaxy.java

--- MirichProjectFiles/LogicClasses/Galaxy.java
+++ MirichProjectFiles/LogicClasses/Galaxy.java
@@ -1,56 +1,115 @@
 package LogicClasses;
 
+	/**********
+	 * class Galaxy
+	 *
+	 * The galaxy class is a blueprint for the different galaxies in the
+	 * solar system and its components like the stars and some of their names
+	 *  
+	 * It contains the following instance variables:
+	 * galaxyName: a String which identifies the galaxy
+	 * starName: a String which identifies the some of the stars in the galaxy
+	 * galaxyRadius: a double which gives the radius of the galaxy in lightyears
+	 * ID: a String type constant 
+	 *
+	 */
 public class Galaxy {
 	
   	private String galaxyName;
 	private String starName;
 	public double galaxyRadius; // In lightyears
-	private final String ID = "0004";
+//	private final String ID = "0004";
 	
-    public Galaxy() {
-    	this.galaxyName = "";
-        this.starName = "";
-        
-        
-    }
-       
-    
-	public Galaxy( String aGalaxyName, String aStarName) {
-		this.galaxyName = aGalaxyName;
-	        this.starName = aStarName;
-	        ;}
+       /* Galaxy(String, String, double)
+	* Constructor for the class
+	*
+	* @param aGalaxyName The name of the galaxy
+	* @param aStarName The name of the star in the galaxy
+	* @param aGalaxyRadius The radius of the galaxy given 
+	*
+	*/    
+	public Galaxy( String aGalaxyName, String aStarName, double aGalaxyRadius) {
+		this.setGalaxyName(aGalaxyName);
+	        this.setStarName(aStarName);
+		this.setGalaxyRadius(aGalaxyRadius);
+	        }
 	
+	/*************
+	 * displayMenu()
+	 *
+	 * @return the displayed menu for the application 
+	 *
+	 */
 	public String displayMenu() {
 		String displayedMenu = "THIS IS THE MILKY WAY MENU! ";
 		return displayedMenu;
 	}
 	
-	public void setStarInfo(String aStarName){
-		this.starName = aStarName;}
-	// need a hashmap here
+	/*************
+	 * setStarName() 
+	 * setter method for the star name
+	 * @param aStarName the star name to be set
+	 *
+	 */
+	public void setStarName(String aStarName){
+		this.starName = aStarName;
+	}
 	
+	/*************
+	 * getGalaxyName() 
+	 * getter method for the galaxy name
+	 * @return the galaxy name 
+	 *
+	 */
 	public  String getGalaxyName() {
 		return this.galaxyName;
 	}
 	
+	/*************
+	 * getGalaxyRadius() 
+	 * getter method for the galaxy radius
+	 * @return the galaxy radius
+	 *
+	 */
 	public double getGalaxyRadius() {
 		return this.galaxyRadius;
 	}
 	
-        public String getStarInfo(){
+	/*************
+	 * getStarName() 
+	 * getter method for the star name
+	 * @return the star name
+	 *
+	 */
+        public String getStarName(){
 	return starName;
-	
-	// info is a variable for the info got from hashmap
 	}
 	
-	public void setGalaxyInfo(String aGalaxyName){
+	/*************
+	 * setStarName() 
+	 * setter method for the star name
+	 * @param aStarName the star name to be set
+	 *
+	 */
+	/*************
+	 * setGalaxyName() 
+	 * setter method for the galaxy name
+	 * @param aGalaxyName the galaxy name to be set
+	 *
+	 */
+	public void setGalaxyName(String aGalaxyName){
 		this.galaxyName = aGalaxyName;
 		}
-	// need a hashmap here
 	
-	public String getGalaxyInfo(){
-	return galaxyName;
-	// info is a variable for the info got from hashmap
+	/*************
+	 * setGalaxyRadius() 
+	 * setter method for the galaxy radius
+	 * @param aGalaxyRadius the galaxy radius to be set
+	 *
+	 */
+	public void setGalaxyRadius(double aGalaxyRadius) {
+		this.galaxyRadius= aGalaxyRadius;
 	}
+	
 	
 }
