f860cb8a358ba973b96c758e8a5bbd8ae04de27b
rashimahendra <60163773+rashimahendra@users.noreply.github.com>
2020-03-25 20:42:46 -0600

    Update MilkyWayGUI.java

--- MirichProjectFiles/GUI/MilkyWayGUI.java
+++ MirichProjectFiles/GUI/MilkyWayGUI.java
@@ -12,12 +12,35 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/* class MilkyWayGUI
+ * This class is a GUI version of the MilkyWay text based class which creates buttons namely
+ * Search and Back which lets the user search by putting the keywords and to go back to the 
+ * previous menu. It also creates a label asking the user a question and giving him the 
+ * possible keywords that can be entered in the search bar
+ * 
+ */
 public class MilkyWayGUI extends Application {
 
+      /****** 
+       * Main method of the class
+       *
+       */
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
+	
+       /********
+   	* start(Stage)
+  	*
+   	* This method sets up a stage and aligns the buttons properly 
+   	* and a text field to display the label asking the user to input the proper 
+	* keywords in the search bar.
+   	*
+   	* @param primaryStage the stage needed for displaying the scenes
+   	* @throws the Exception of any nature that might occur
+   	*
+   	*/
 	public void start(Stage primaryStage) throws Exception {
 		
 		Button searchButton = new Button("Search");
