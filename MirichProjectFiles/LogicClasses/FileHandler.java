2e149979df93116890f205386968bfae4b35ba8b
GulyaTurner <60667443+GulyaTurner@users.noreply.github.com>
2020-03-31 14:44:04 +0400

    Update FileHandler.java

--- MirichProjectFiles/LogicClasses/FileHandler.java
+++ MirichProjectFiles/LogicClasses/FileHandler.java
@@ -1,13 +1,21 @@
 package LogicClasses;
 import java.io.File;
 import java.util.Scanner;
-
+/*class FileHandler
+ * This class is a Logic version of the FileHandler text based class which 
+ * handles the files and if not found displays that the file is not found
+ * 
+ * Instance variable:
+ * myScanner: for input
+ */
 public class FileHandler {
 	
-		private Scanner myScanner;
+		private Scanner myScanner;		
 		
-		
-		
+		/* readFiles(String theTextFile)
+		* constructor that reads files
+		* @param String theTextFile text file
+		*/
 		public void readFiles(String theTextFile) {
 				
 			while(myScanner.hasNext()) {
@@ -20,11 +28,15 @@ public class FileHandler {
 			
 		}
 		
-		
+		// get Scanner
 		public Scanner getScanner() {
 			return this.myScanner;
 		}
-		
+		/* openFiles(String theTextFile)
+		* constructor that opens and checks files 
+		* @param theTextFile text file
+		* when file not found displays the message below
+		*/
 		public void openFiles(String theTextFile) {
 			
 			try {
@@ -34,7 +46,7 @@ public class FileHandler {
 			}
 		
 			catch(Exception e) {
-				
+				// display of message
 				System.out.println("File not found");
 				
 			}
