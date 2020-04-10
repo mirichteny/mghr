c2ed0db5ad06a8b4ff1144b169977d661ea8b8d1
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-04-09 19:36:23 -0600

    AddedVenusClass

---
+++ GroupProjectCodeFiles/Venus.java
@@ -0,0 +1,19 @@
+/* Venus Is-a Planet. (Inherits methods from planet)
+*
+*
+*/
+public class Venus extends Planet {
+					// instance variables of Venus
+					private double venusMass = 4.867*Math.pow(10, 24); //kg
+					private double venusRadius = 6051800; //m/s
+					private double distanceFromSun = 0.723; //in Astronomical Units (AU) 
+					
+					public Venus(double aPlanetMass, double aPlanetRadius) {
+						super(aPlanetMass, aPlanetRadius);
+						
+					}
+					
+					public Venus(Venus toCopy) {
+						super(toCopy);
+					}
+}
