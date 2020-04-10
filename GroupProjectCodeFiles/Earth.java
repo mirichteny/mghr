9c6420da64de7585266fee9af22889261d787c0f
rashimahendra <60163773+rashimahendra@users.noreply.github.com>
2020-04-09 18:10:06 -0600

    Update Earth.java

--- GroupProjectCodeFiles/Earth.java
+++ GroupProjectCodeFiles/Earth.java
@@ -1,6 +1,22 @@
 import java.util.HashMap;
 import java.util.ArrayList;
-public class Earth extends Planets {
+/* class Earth
+ * This class is a Logic version of the Earth text based class which provides user
+ * the information of itself
+ * 
+ * It contains the following instance variables:
+ * PLANET_TYPE: type of planet
+ * earthProperties: list of Earth properties
+ * mass: mass of planet
+ * gravity: gravity of planet
+ * planetRadius: radius of planet
+ * planetsOrder: position in solar system of planets relative to the sun
+ * orbitalPeriod: orbital time of Earth 
+ * countries: list of countries
+ * continents: list of continents
+ * age: age of Earth
+ */
+public class Earth extends Planet {
 
 	
 				
@@ -10,7 +26,7 @@ public class Earth extends Planets {
 				private final double mass = 5.972*Math.pow(10,24);
 				private final double gravity = 9.81; // m/s^2
 				private final double planetRadius = 6.3*Math.pow(10, 4);
-				private int planetsOrder = 3; //Position in solar system of planets relative to the sun
+				private int planetsOrder = 3; 
 				private double orbitalPeriod = 365.25; // days
 				private ArrayList<String> countries;
 				private HashMap <String, String> continents = new HashMap<>();
@@ -20,30 +36,36 @@ public class Earth extends Planets {
 				public Earth() {
 					
 				}
-				
+				/*Earth(double aPlanetRadius, double aPlanetMass)
+				* constructor for the class
+				*
+				* gets information from the following parameters
+				* @param double aPLanetRadius radius of planet
+				* @param double aPlanetMass mass of planet
+				*/
 				public Earth(double aPlanetRadius, double aPlanetMass) {
 					super(aPlanetRadius, aPlanetMass);
 				}
 				
-				
+				// get Orbital Period
 				public double getOrbitalPeriod() {
 					
 					return this.orbitalPeriod;
 				}
 				
 				
-				
+				// get countries
 				public ArrayList<String> getCountries(){
 					return this.countries;
 				}
 				
 				
-				
+				// display menu
 				public String displayMenu() {
 					return "";
 				}
 				
-				
+				// set Earth properties
 				public void addProperties(String aProperty) {
 					
 					this.earthProperties.add(aProperty);
