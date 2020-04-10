09112fd24f5954006be2ece123fc98128ac6ccca
rashimahendra <60163773+rashimahendra@users.noreply.github.com>
2020-04-09 18:24:21 -0600

    Update and rename Planets.java to Planet.java

--- GroupProjectCodeFiles/Planet.java
+++ GroupProjectCodeFiles/Planet.java
@@ -1,55 +1,93 @@
 
-public class Planets {
+public class Planet {
 	/*
-	 * The planets class is a blueprint for all the planets in the solar system including earth.
+	 * The planet class is a blueprint for all the planets in the solar system including earth.
 	 * 
-	 * 
-	 * 
-	 * 
+	 * It contains the following instance variables:
+	 * materials: a String type array containing the list of materials
+	 * G: a double type gravitational constant 
+	 * gravity: a double type variable which stores the gravity of the planets
+	 * planetMass: a double type variable which stores the mass of the planets 
+	 * planetRadius: a double type variable which stores the radius of the planets 
+	 * ID: a String type constant 
 	 * 
 	 */
 					
 					private String[] materials;
-					private double age;
 					private final double G = 6.6742*Math.pow(10,-11 ); //Gravitational constant
 					private double gravity; // m/s^2
 					private double planetMass;
 					private double planetRadius;
 					private final String ID = "0003";
-					
-			public Planets() {
+			
+			/****
+			 * a default constructor of the class Planet() which sets the gravity
+			 * and the mass of the planets 
+			 *
+			 */
+			public Planet() {
 				this.gravity = G*planetMass*(1/Math.pow(planetRadius, 2)); // Equation for gravity of a planet
 				this.planetMass = 5.683*Math.pow(10,26); //Average planet mass
 			}
 			
-			public Planets(double aPlanetRadius, double aPlanetMass) {
+			/****
+			 * Planet(double, double)
+			 * a constructor of the class Planet() which sets the gravity
+			 * and the mass of the planets 
+			 *
+			 */
+			public Planet(double aPlanetRadius, double aPlanetMass) {
 				this.gravity = G*planetMass*(1/Math.pow(planetRadius, 2)); // Equation for gravity of a planet
 				this.planetMass = 5.683*Math.pow(10,26); //Average planet mass
 			}
 			
+			/***
+			 * getGravity()
+			 * getter method for gravity of the planet
+			 * @return gravity returns gravity
+			 *
+			 */
 			public double getGravity() {
 				
 				return gravity;
 				
 			}
 			
+			/***
+			 * getPlanetMass()
+			 * getter method for mass of the planet
+			 * @return planetMass returns mass of the planet
+			 *
+			 */
 			public double getPlanetMass() {
 				return planetMass;
 			}
 			
+			/***
+			 * getMaterials()
+			 * getter method for materials of the planet
+			 * @return materials returns materials
+			 *
+			 */
 			public String[] getMaterials() {
 				return materials;
 			}
 			
+			/***
+			 * getPlanetRadius()
+			 * getter method for radius of the planet
+			 * @return planetRadius returns radius of the planet 
+			 *
+			 */
 			public double getPlanetRadius() {
 				return planetRadius;
 			}
 			
-			/*
+			/* getPlanetDescription()
 			 * This method returns a description of the planet such as surface material,age
 			 * number of moons, etc.
 			 * 
-			 * 
+			 * @return description returns the description of the planet
 			 */
 			public String getPlanetDescription() {
 				
@@ -61,6 +99,12 @@ public class Planets {
 				return description;
 			}
 			
+			/***
+			 * displayMenu()
+			 * This method displays the planet menu on the screen
+			 * @return output returns output
+			 *
+			 */
 			public String displayMenu() {
 				Grid g = new Grid(5, 60,"X");
 				String output =  "THIS IS THE PLANET MENU ";
