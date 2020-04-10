fdb45cea05452356e02c01a122dab4e05518279d
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-04-09 19:30:12 -0600

    Updated

--- MirichProjectFiles/LogicClasses/Venus.java
+++ MirichProjectFiles/LogicClasses/Venus.java
@@ -1,7 +1,20 @@
 package LogicClasses;
-
-public class Venus extends Planet{
-
-				
-		
+/* Venus Is-a Planet. (Inherits methods from planet)
+*
+*
+*/
+public class Venus extends Planet {
+					// instance variables of Venus
+					private double venusMass = 4.867*Math.pow(10, 24); //kg
+					private double venusRadius = 6051800; //m/s
+					private double distanceFromSun = 0.723; //in Astronomical Units (AU) 
+					
+					public Venus(double aPlanetMass, double aPlanetRadius) {
+						super(aPlanetMass, aPlanetRadius);
+						
+					}
+					
+					public Venus(Venus toCopy) {
+						super(toCopy);
+					}
 }
