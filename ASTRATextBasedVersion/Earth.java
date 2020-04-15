0ec2b1889435d967a0e1dd638155f332b4993f05
mirichteny <52932404+mirichteny@users.noreply.github.com>
2020-04-15 14:20:28 -0600

    FinalTextBasedVersion
    This is the final product of the text based version. See if it compiles guys.

---
+++ ASTRATextBasedVersion/Earth.java
@@ -0,0 +1,79 @@
+
+import java.util.HashMap;
+import java.util.ArrayList;
+/* class Earth
+ * This class is a Logic version of the Earth text based class which provides user
+ * the information of itself
+ * 
+ * It contains the following instance variables:
+ * PLANET_TYPE: type of planet
+ * earthProperties: list of Earth properties
+ * mass: mass of planet
+ * gravity: gravity of planet
+ * planetRadius: radius of planet
+ * planetsOrder: position in solar system of planets relative to the sun
+ * orbitalPeriod: orbital time of Earth 
+ * countries: list of countries
+ * continents: list of continents
+ * age: age of Earth
+ */
+public class Earth extends Planet {
+
+	
+				
+	
+				private final String PLANET_TYPE = "terrestrial";
+				private ArrayList<String> earthProperties = new ArrayList<String>();
+				private final double mass = 5.972*Math.pow(10,24);
+				private final double gravity = 9.81; // m/s^2
+				private final double planetRadius = 6.3*Math.pow(10, 4);
+				private int planetsOrder = 3; 
+				private double orbitalPeriod = 365.25; // days
+				private ArrayList<String> countries;
+				private HashMap <String, String> continents = new HashMap<>();
+				private double age;
+				
+			
+				/*Earth(double aPlanetRadius, double aPlanetMass)
+				* constructor for the class
+				*
+				* gets information from the following parameters
+				* @param double aPLanetRadius radius of planet
+				* @param double aPlanetMass mass of planet
+				*/
+				public Earth(double aPlanetRadius, double aPlanetMass) {
+					super(aPlanetRadius, aPlanetMass);
+				}
+				
+				// get Orbital Period
+				public double getOrbitalPeriod() {
+					
+					return this.orbitalPeriod;
+				}
+				
+				
+				// get countries
+				public ArrayList<String> getCountries(){
+					return this.countries;
+				}
+				
+				
+				// display menu
+				public String displayMenu() {
+					return "";
+				}
+				
+				// set Earth properties
+				public void addProperties(String aProperty) {
+					
+					this.earthProperties.add(aProperty);
+					
+					
+				}
+				
+				
+				
+				
+				
+				
+}
---
+++ ASTRATextBasedVersion/MilkyWay.java
@@ -0,0 +1,67 @@
+
+/*
+ * MilkyWay Is-a Galaxy. (Inherits methods from galaxy)
+ * 
+ * 
+ */
+public class MilkyWay extends Galaxy {
+									
+						
+						/* MilkyWay(String aGalaxyName, double aRadius) constructor
+						* inherits the following parameters
+						*
+						* @param aGalaxyName name of the galaxy
+						* @param aRadius radius
+						*/
+						public MilkyWay(String aGalaxyName, double aRadius) {
+							super(aGalaxyName, aRadius);
+							
+						}
+                                               /***
+						* MilkyWay()
+						* Default constructor
+						*/
+						public MilkyWay() {
+							
+						}
+						
+						/***
+						* MilkyWay(MilkyWay)
+						* @param m: a MilkyWay input 
+						* Copy constructor
+						*/
+						public MilkyWay(MilkyWay m) {
+							this();
+						}
+	
+						/*
+						* displayMenu()
+						* shows display menu with the message below and returns output
+						* @returns output
+						*
+						*/
+						public String displayMenu() {
+							
+							String output = "THIS IS THE MILKY WAY MENU!";
+							
+							return output;
+						
+						}
+						/* getGalaxyDesc()
+						*
+						* gives galaxy description to user
+						* @returns desc: the description of the galaxy
+						*
+						*/
+						public String getGalaxyDesc() {
+							
+							String word1 = "";
+							String word2 = "";
+							String word3 = "";
+							
+							String desc = "The milky way galaxy is the galaxy we are in. " + " It contains " + word1  + " . It would take  " + "   lightyears " +
+							" just to the center which contains a black hole ";
+							
+							return desc;
+						}
+}
